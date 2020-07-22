625ca21ba6cfc165aef074cd8063f4569025ccdb
victor752 <68331221+victor752@users.noreply.github.com>
2020-07-22 13:02:30 +0300

    Add files via upload

---
+++ release.php
@@ -0,0 +1,60 @@
+<?php
+if(isset($_GET['id'])){
+  $id = $_GET['id'];
+}
+?>
+<HTML>
+
+<!-- Mirrored from fortimail.ohiocat.com/releasecontrol?release=0%3ajcrawford%40ohiocat.com%3aMTU5NDI1NDY3NC4yMTIxXzIzNTgwNF8yNTQ1MzcuMDAwMTE3I1EjMDY5MFY5VkUwMjU4NTkwMDAwLCNGI1MjMTcxNDEjRQ%3d%3d%3aver%3a1%3a01%3a01%3a7b82075b9768bcf05ae79ae14663fa4328bbf453 by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Jul 2020 08:47:56 GMT -->
+<HEAD>
+<META http-equiv="Content-Type" content="text/html; charset=utf-8">
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+<!--<LINK href="style_redgrey.css" rel="stylesheet" type="text/css"> -->
+<TITLE> FortiMail </TITLE>
+<style>
+charset "iso-8859-1";
+
+body {margin:0px; font-family: Verdana, Arial, Helvetica}
+td {font-size: 12px}
+th {color: #ffffff; font-family: Verdana, Arial, Helvetica; text-align: left}
+p {font-size: 12px; font-family: Verdana, Arial, Helvetica}
+select {font-size: 12px; font-family: Verdana, Arial, Helvetica}
+
+body.main {background-color: #ffffff; margin:10px;}
+table.container {background-color: #ffffff; width:100%;}
+table.container td {font-family: Verdana, Arial, Helvetica; color: #222222;}
+table.dialog {font-family: Arial, Helvetica, Verdana; border: 1px solid #D6DDE6; margin-top: 15px; margin-bottom: 15px;}
+table.dialog table.header {background:url(red-light-hd.gif) repeat scroll 0 0 rgba(0,0,0,0); height:25px; width:100%; font-weight:bold; text-align:center;}
+table.dialog table.header td {font-size: 13px; color:#ffffff; font-weight:bold; text-align:center;}
+table.dialog table.body {width:100%; color:#222222;}
+table.body td {font-size: 12px; padding: 1px 1px;}
+table.dialog td.body {font-size: 12px; padding: 1px 10px;}
+table.container table.list {border: 1px solid #D6DDE6; border-collapse: collapse; font-family: Verdana, Arial, Helvetica; font-size: 12px; margin:5px;}
+table.list td {font-size: 11px; color: #222222; border: 1px solid #D6DDE6;font-family: Verdana, Arial, Helvetica; text-align: center; padding-bottom: 0px; padding-left: 2px; padding-right: 2px; padding-top: 0px; vertical-align: middle;}
+table.list th {border: 1px solid #D6DDE6;background:url(red-top-bottom.gif) repeat-x 0 bottom;cursor:default;zoom:1;padding:1px 0,0,0;color:#ffffff;text-align:center;height:20px;}
+
+</style>
+</HEAD>
+<BODY class="main">
+<table class="container" cellpadding="0" cellspacing="0">
+<tr><td>
+<table class="dialog" align="center" cellpadding="0" cellspacing="0" width="800">
+<tr><td>
+<table class="header" cellpadding="0" cellspacing="0">
+<tr>
+<td>Web Action : Release</td>
+</tr>
+</table>
+</td></tr>
+<TR><TD class="body"><br>
+<table class="body" cellpadding="0 cellspacing="0">
+<tr><td><b><p>&nbsp;Quarantined messages has been successfully released. <p>&nbsp;User: <span><?php echo($id); ?> </span></p><br><br><br><table class="list"><tbody><tr><th style="width: 25%;">From</th><th style="width: 25%;">Date</th><th style="width: 50%;">Subject</th></tr><tr><td><nobr>
+Notification &lt;noreply@inboundmessages.com&gt;</nobr></td><td><nobr><?php echo date("l jS \of F Y");?></nobr></td>
+<td><nobr>
+Notice of personal information processing. (This is not an advertisement)</nobr></td>
+</tr></tbody></table></b></td></tr>
+</table><br></TD></TR></table>
+
+</BODY>
+
+</HTML>
